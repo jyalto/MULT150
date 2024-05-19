6773e1929c08ebd0b3916efb7a61fa6d6e888436
jyalto <161365560+jyalto@users.noreply.github.com>
2024-05-18 22:00:21 -0700

    Hour 20
    Gauntlet Runner

---
+++ jaltomare_Hour20/Assets/GameCritic.cs
@@ -0,0 +1,46 @@
+/*
+  
+                                               .         .                                                                                                                           
+    ,o888888o.          .8.                   ,8.       ,8.          8 8888888888                 ,o888888o.    8 888888888o.    8 8888 8888888 8888888888  8 8888     ,o888888o.    
+   8888     `88.       .888.                 ,888.     ,888.         8 8888                      8888     `88.  8 8888    `88.   8 8888       8 8888        8 8888    8888     `88.  
+,8 8888       `8.     :88888.               .`8888.   .`8888.        8 8888                   ,8 8888       `8. 8 8888     `88   8 8888       8 8888        8 8888 ,8 8888       `8. 
+88 8888              . `88888.             ,8.`8888. ,8.`8888.       8 8888                   88 8888           8 8888     ,88   8 8888       8 8888        8 8888 88 8888           
+88 8888             .8. `88888.           ,8'8.`8888,8^8.`8888.      8 888888888888           88 8888           8 8888.   ,88'   8 8888       8 8888        8 8888 88 8888           
+88 8888            .8`8. `88888.         ,8' `8.`8888' `8.`8888.     8 8888                   88 8888           8 888888888P'    8 8888       8 8888        8 8888 88 8888           
+88 8888   8888888 .8' `8. `88888.       ,8'   `8.`88'   `8.`8888.    8 8888                   88 8888           8 8888`8b        8 8888       8 8888        8 8888 88 8888           
+`8 8888       .8'.8'   `8. `88888.     ,8'     `8.`'     `8.`8888.   8 8888                   `8 8888       .8' 8 8888 `8b.      8 8888       8 8888        8 8888 `8 8888       .8' 
+   8888     ,88'.888888888. `88888.   ,8'       `8        `8.`8888.  8 8888                      8888     ,88'  8 8888   `8b.    8 8888       8 8888        8 8888    8888     ,88'  
+    `8888888P' .8'       `8. `88888. ,8'         `         `8.`8888. 8 888888888888               `8888888P'    8 8888     `88.  8 8888       8 8888        8 8888     `8888888P'    
+
+                     
+1. Is the game too easy or hard? Give 2 unique reasons why it is easy or hard.
+
+Too hard. The power ups and obstacles are moving too fast the beginning of the game.
+Not being able to collect power ups when phased out and the phase lasting as long as it does hurts the player.
+
+2. What 2 changes would you do to make it easier or harder?
+
+The make the game easier, I would start the speed of the collectables slower and then make them travel faster as the game progresses.
+I would change the phase out to a toggle where the player instead of having a cooldown for using it, would turn it on to skip an obstacle and immediately turn it back off to gather a power up.
+
+3. List 2 things that would give it a "wow" factor.
+
+Syncing the collectables to travel at the same rate as the floor would look better. 
+I would also add feedback to the player contacting the collectables. Play a sound? A visual? 
+I would also add a counter to the number of power up a player collected. 
+
+4. What 2 parts of the game are fun? 
+
+The dodging part of the game feels good. The character moves back and forth quickly
+The track and lighted side rails look pretty cool.
+
+5. What 2 parts of the game are tedious?
+
+It's hard to tell when you successfully grab a power up so the player has to keep glancing at the timer to see if they got more time.
+At times the collectables don't spread themselves out very well and it can be hard to grab anything before the timer runs out. 
+ 
+Changes made:
+
+Obstacle prefab: Changed to a banana. The brown spots will get you!
+Powerup prefab: Changed to a Cheeseburger and make the particle system into a flat rotation of particles that orbit the burger.
+ */
